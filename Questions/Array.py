8805e05d9534cfdce249e35371bf6560755f56f1
Amandeep <184535622+Amandeep006@users.noreply.github.com>
2026-07-13 23:12:27 +0530

    array

--- Questions/Array.py
+++ Questions/Array.py
@@ -27,12 +27,69 @@ a reverse pair is a pair (i,j).
 """
 The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
 """
-num=[1,3,5,7,9]
-sum=0
-cum_sum=[]
-for i in num:
-    sum+=i
-    cum_sum.append(sum)
+# num=[1,3,5,7,9]
+# sum=0
+# cum_sum=[]
+# for i in num:
+#     sum+=i
+#     cum_sum.append(sum)
 
-print(f"Cumulative Sum: {cum_sum}")
+# print(f"Cumulative Sum: {cum_sum}")
+
+"""
+POSITIVE CUMULATIVE SUM
+The cumulative sum of an array at index i is defined as the sum of all elements of the array from index 0 to index i.
+"""
+
+# num=[1,-2,3,4,-6]
+# sum=0
+# cum_sum=[]
+
+# for i in num:
+#     sum+=i
+#     cum_sum.append(sum)
+
+# print(f"Cumulative Sum : {cum_sum}")
+
+
+"""
+Identical twins
+For an array of integers nums, an identical twin is defined as pair (i, j) where nums[i] is equal to nums[j] and i < j.
+"""
+
+# num=[1,2,2,3,2,1]
+# count=0
+# show=[]
+# index=[]
+
+# for i in range(len(num)-1):
+#     for j in range(i+1,len(num)):
+#         if num[i]==num[j]:
+#             count+=1
+#             show.append([num[i],num[j]])
+#             index.append((i,j))
+
+# print(f"Number of Identical Twins : {count}")
+# print(f"Identical Twins : {show}")
+
+# index=tuple(index) # There is an inbuild function to convert list into tuple.
+# print(f"Indexes: {index}")
+
+
+"""
+Given an array of integers, find the elements which have an even number of digits.
+"""
+
+# arr = [42,564,5775,34,123,454,1,5,45,3556,23442]
+
+# even_digits = []
+
+# for num in arr:
+#     if len(str(abs(num))) % 2 == 0: # abs is used to convert the value for negative to positive number.
+#         even_digits.append(num)
+
+# print(even_digits)
+# print("Count =", len(even_digits))
+
+
 
